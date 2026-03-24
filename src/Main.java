6684ea5e7dfbe4244d6c55af0dee1e311018a6eb
Nat3223 <s33171@pjwstk.edu.pl>
2026-03-24 10:50:06 +0100

    commited

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        // menu items
+        MenuItem m1 = new MenuItem("C-01", "Espresso", 9.0, "coffee");
+        MenuItem m2 = new MenuItem("C-02", "Cappuccino", 13.5, "coffee");
+        MenuItem m3 = new MenuItem("D-01", "Cheesecake", 16.0, "dessert");
+
+        // customer
+        CafeCustomer customer = new CafeCustomer(100, "Julia", "Mazur", "j.mazur@mail.pl");
+
+        // order
+        Order order = new Order(customer);
+        order.addItem(m1);
+        order.addItem(m2);
+        order.addItem(m3);
+
+        // prints
+        System.out.println(customer);
+        System.out.println(order);
+
+        System.out.println("Total value: " + order.calculateTotal());
+        System.out.println("Number of items: " + order.countItems());
+        System.out.println("Products created: " + MenuItem.getProductCount());
+
+        // equals test
+        MenuItem copy = new MenuItem("C-01", "Espresso copy", 9.0, "coffee");
+        System.out.println("MenuItem equals: " + m1.equals(copy));
+
+        CafeCustomer sameCustomer = new CafeCustomer(100, "Julia", "Mazur", "test@mail.pl");
+        System.out.println("Customer equals: " + customer.equals(sameCustomer));
+
+        // mark paid
+        order.markAsPaid();
+        System.out.println(order);
     }
 }
